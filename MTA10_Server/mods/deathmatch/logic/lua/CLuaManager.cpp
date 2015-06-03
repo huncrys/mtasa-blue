bd8af257bf8d9c78aa8e6663d863e921db51ce7e
Crys <crys@crys.hu>
2015-06-03 14:17:41 +0200

    Applied LuaJIT patch by eeew.

--- MTA10_Server/mods/deathmatch/logic/lua/CLuaManager.cpp
+++ MTA10_Server/mods/deathmatch/logic/lua/CLuaManager.cpp
@@ -45,8 +45,8 @@ CLuaManager::CLuaManager ( CObjectManager* pObjectManager,
 
     // Load our C Functions into LUA and hook callback
     LoadCFunctions ();
-    lua_registerPreCallHook ( CLuaDefs::CanUseFunction );
-    lua_registerUndumpHook ( CLuaMain::OnUndump );
+    //lua_registerPreCallHook ( CLuaDefs::CanUseFunction );
+    //lua_registerUndumpHook ( CLuaMain::OnUndump );
 
 #ifdef MTA_DEBUG
     // Check rounding in case json is updated
