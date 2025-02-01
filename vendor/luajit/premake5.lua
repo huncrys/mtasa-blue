ab5517898463f465f6b7b93ee73ae1f334e2f1e5
Kristof Bach <crys@crys.hu>
2025-02-01 15:08:46 +0100

    Fix LuaJIT cross compilation

--- vendor/luajit/premake5.lua
+++ vendor/luajit/premake5.lua
@@ -1,6 +1,16 @@
 project "LuaJIT"
 	kind "Makefile"
 
-	buildcommands { "cd ../vendor/luajit && make clean && make" }
-	rebuildcommands { "cd ../vendor/luajit && make clean && make" }
-	cleancommands { "cd ../vendor/luajit && make clean" }
+	local luajit_make = "@+${MAKE} -C ../vendor/luajit"
+	if os.getenv("LUAJIT_CC") then
+		luajit_make = luajit_make .. " CC=" .. os.getenv("LUAJIT_CC")
+	end
+	if _OPTIONS["gccprefix"] then
+		luajit_make = luajit_make .. " CROSS=" .. _OPTIONS["gccprefix"]
+	end
+	buildcommands { luajit_make }
+	rebuildcommands { 
+		luajit_make .. " clean",
+		luajit_make
+	}
+	cleancommands { luajit_make .. " clean" }
