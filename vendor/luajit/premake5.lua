d334af782fdfe411334846254f61d6d7bc5e3a38
Kristof Bach <crys@crys.hu>
2025-02-02 15:16:29 +0100

    Build with GitLab CI

--- vendor/luajit/premake5.lua
+++ vendor/luajit/premake5.lua
@@ -1,6 +1,23 @@
+local luajit_make = "@+${MAKE} -C ../vendor/luajit BUILDMODE=static XCFLAGS=-fPIC TARGET_T=libluajit.a"
+if os.getenv("LUAJIT_CC") then
+	luajit_make = luajit_make .. " CC=\"" .. os.getenv("LUAJIT_CC") .. "\""
+end
+if os.getenv("LUAJIT_HOST_CC") then
+	luajit_make = luajit_make .. " HOST_CC=\"" .. os.getenv("LUAJIT_HOST_CC") .. "\""
+end
+if _OPTIONS["gccprefix"] then
+	luajit_make = luajit_make .. " CROSS=\"" .. _OPTIONS["gccprefix"] .. "\""
+end
+
 project "LuaJIT"
 	kind "Makefile"
 
-	buildcommands { "cd ../vendor/luajit && make clean && make" }
-	rebuildcommands { "cd ../vendor/luajit && make clean && make" }
-	cleancommands { "cd ../vendor/luajit && make clean" }
+	buildcommands {
+		luajit_make .. " clean",
+		luajit_make
+	}
+	rebuildcommands { 
+		luajit_make .. " clean",
+		luajit_make
+	}
+	cleancommands { luajit_make .. " clean" }
